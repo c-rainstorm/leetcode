0b0856704a8d4cdacd943b5328c21c026bb3e8da
Mr.恒尘 <rainstorm.me@outlook.com>
2021-03-15 15:24:33 +0800

    [A] 387

---
+++ src/main/java/s0300/N0387FirstUniqueCharacterInAString.java
@@ -0,0 +1,20 @@
+package s0300;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class N0387FirstUniqueCharacterInAString {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> frequency = new HashMap<>();
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            frequency.put(ch, frequency.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); ++i) {
+            if (frequency.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
